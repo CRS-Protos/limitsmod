65388ff3c0c6af8e9cc49276b1c8d7a9a25e7eb4
CRS-Protos <geekygamerschardt@gmail.com>
2026-03-16 23:19:25 -0500

    fixed vial of cleansing

--- src/main/java/net/crs/limits/item/custom/CleansingVial.java
+++ src/main/java/net/crs/limits/item/custom/CleansingVial.java
@@ -1,6 +1,8 @@
 package net.crs.limits.item.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.item.ItemEntity;
@@ -26,24 +28,26 @@ public class CleansingVial extends Item
             BlockPos posclicked = pContext.getClickedPos();
             Player player = pContext.getPlayer();
             assert player != null;
-            ItemStack itemstack = player.getUseItem();
+            ItemStack itemstack = player.getItemInHand(InteractionHand.MAIN_HAND);
             BlockState state = pContext.getLevel().getBlockState(posclicked);
 
             if (state.is(Blocks.SOUL_SAND))
             {
-                pContext.getLevel().setBlock(posclicked, Blocks.SAND.defaultBlockState(), 3);
-                itemstack.shrink(1);
-                //playsound()
+                //change block and subtract item stack by 1
+                use(pContext, player, posclicked, itemstack);
             }
         }
 
         return InteractionResult.SUCCESS;
     }
 
-    public void playsound()
+    public void use(UseOnContext pContext, Player player, BlockPos posclicked, ItemStack itemstack)
     {
-
+        //player.sendSystemMessage(Component.literal(itemstack.toString()));
+        pContext.getLevel().setBlock(posclicked, Blocks.SAND.defaultBlockState(), 3);
+        if (!player.getAbilities().instabuild) {
+            itemstack.shrink(1);
+        }
+        //playsound
     }
-
-
 }
